1d58c0303b760bf4db5a1f538ee061793613f8fa
Lorenzo Bettini <lorenzo.bettini@gmail.com>
2019-02-25 20:51:24 +0100

    created first test case

---
+++ testing.example/tests/testing/example/bank/BankAccountTest.java
@@ -0,0 +1,14 @@
+package testing.example.bank;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class BankAccountTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
